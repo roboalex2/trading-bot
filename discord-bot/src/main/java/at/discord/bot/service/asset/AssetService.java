1182c436b57b34ae8571037cebb8fdcdd151e564
joni97k <jknapp@alpenweb.net>
2025-01-12 19:45:13 +0100

    Add @Service annotation in AssetService to fix failed build process (no Bean)

--- discord-bot/src/main/java/at/discord/bot/service/asset/AssetService.java
+++ discord-bot/src/main/java/at/discord/bot/service/asset/AssetService.java
@@ -1,5 +1,8 @@
 package at.discord.bot.service.asset;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AssetService {
     public String getUserAssets(String userId) {
         return userId;
